5de8034c17a97afae8cc53016ab683081ccd3f51
Emmanuel Yapura <eyapura96@gmail.com>
2025-06-13 21:00:07 -0300

    modifica nombre btn cookies

--- scraper/cookies.py
+++ scraper/cookies.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 # Cerrar cartel de código postal
 def aceptar_cookies(driver):
     try:
-        btn_close = WebDriverWait(driver, 10).until(
+        btn_confirmacion_cookies = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, 'p.location-confirmation-modal__modal-close'))
         )
-        btn_close.click()
+        btn_confirmacion_cookies.click()
     except Exception as e:
         print("No se encontró el cartel o ya estaba cerrado.")
